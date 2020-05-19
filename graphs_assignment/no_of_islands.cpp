366e68f21bde5a534310158ab7beda81b85cabfa
Ishan Shanware <Ishan.Shanware@iiitb.org>
2020-05-19 22:22:03 +0530

    islands leetcode done

--- graphs_assignment/no_of_islands.cpp
+++ graphs_assignment/no_of_islands.cpp
@@ -1,10 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+//idea is calculate number of connected components
+//again we use the array of row and column opertaion 
+//on moving up, down , right , left 
 
+//we use dfs to calculate the number of connected components
 int row_op[4] = {-1, 1, 0, 0};
 int col_op[4] = {0, 0, 1, -1};
 
-void dfs(int r, int c, int rows, int cols, vector<vector<char>>& grid, vector<vector<bool>>& visited){
+void dfs(int r, int c, int rows, int cols, vector<vector<char>>& grid, vector<vector<bool>>& visited){ //recursive  dfs
     visited[r][c] =  true;
     for(int i = 0; i<4; i++){
         if(r+row_op[i]>=0 && r+row_op[i] <rows && c+col_op[i]>=0 && c+col_op[i]<cols){
@@ -16,18 +20,20 @@ void dfs(int r, int c, int rows, int cols, vector<vector<char>>& grid, vector<ve
 }
 
 
+
 int count_components(vector<vector<char>>& grid, vector<vector<bool>>& visited, int rows, int cols){
     int count = 0;
     for(int i=0; i<rows; i++){
         for(int j=0; j<cols; j++){
-            if(grid[i][j] == '1' && visited[i][j] == false){
+            if(grid[i][j] == '1' && visited[i][j] == false){ //if node is unvisited we apply dfs
+            //after the loop if unvisted '1' are left we count them in separate connected components
                 dfs(i, j, rows, cols, grid, visited);
                 count += 1;
             }
         }
     }
     
-    return count;
+    return count; //number of connected components are returned
 }
 
 int numIslands(vector<vector<char>>& grid) {
